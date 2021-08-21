05782e264ef6994b753c7ac6cab5d5b1afa6844f
Caoxuheng <80499410+Caoxuheng@users.noreply.github.com>
2021-08-21 12:35:31 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ with open('README.md','r') as fh:
 
 setuptools.setup(
     name='imgvision',
-    version = '0.0.2',
+    version = '0.0.5',
     author = 'Xuheng Cao',
     author_email = 'caoxuhengcn@gmail.com',
     description = 'A package for image vision',
-    long_description = description ,
+    long_description = 'An image vision package. It may help you achieve spectral image visualization, convert the spectral image to many kinds of color space, such as sRGB, Adobe RGB, and CIE 1964XYZ.'
+                       'Besides, it includes some image process operation, evaluation, downsample, cluster prediction, and so on  ',
     url = 'https://github.com/Caoxuheng/imgvision',
     classifiers = ['License :: OSI Approved :: MIT License',
                    ],
